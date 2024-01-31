48f98873de54ddd235147dc38165c8e761aae6fa
Prajwal Gautam <prajwalgautam08@gmail.com>
2024-01-31 14:00:37 +0545

    Fix: removed unncessary require statement from application.rb

--- config/application.rb
+++ config/application.rb
@@ -13,8 +13,7 @@ require "action_text/engine"
 require "action_view/railtie"
 require "action_cable/engine"
 # require "rails/test_unit/railtie"
-# Asset management 
-require "sprockets/railtie"
+# Asset management
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
