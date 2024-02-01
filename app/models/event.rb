2118fb3750182baba0d416b0ab83b138b31ac2f9
Prajwal Gautam <prajwalgautam08@gmail.com>
2024-02-01 13:03:50 +0545

    Feat: added active storage relation with places and events

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,16 @@
 class Event < ApplicationRecord
+  validates :name, presence: true
+  validates :descrption, presence: true
+  validate :attachments_are_images
+
   belongs_to :place
+  has_many_attached :pictures
+
+  def attachments_are_images
+    return unless pictures.attached?
+
+    pictures.each do |picture|
+      errors.add(:pictures, 'must be images') unless picture.image?
+    end
+  end
 end
--- app/models/place.rb
+++ app/models/place.rb
@@ -1,3 +1,16 @@
 class Place < ApplicationRecord
+  validates :name, presence: true
+  validates :descrption, presence: true
+  validate :attachments_are_images
+
   has_many :events, dependent: :destroy
+  has_many_attached :pictures
+
+  def attachments_are_images
+    return unless pictures.attached?
+
+    pictures.each do |picture|
+      errors.add(:pictures, 'must be images') unless picture.image?
+    end
+  end
 end
