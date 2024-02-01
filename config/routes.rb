aa9500d5be53ad8cb04102af29b820e287a7a93b
Prajwal Gautam <prajwalgautam08@gmail.com>
2024-02-01 14:42:23 +0545

    Fix: routes indentation fixed

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,20 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :admins, default: { format: { with: :json } }, path: '', path_names: {
-                                                                       sign_in: 'login',
-                                                                       sign_out: 'logout',
-                                                                       registration: 'signup'
-                                                                     },
-                      controllers: {
-                        sessions: 'admins/sessions',
-                        registrations: 'admins/registrations'
-                      }
+  devise_for :admins,
+             default: { format: { with: :json } },
+             path: '',
+             path_names:
+             {
+               sign_in: 'login',
+               sign_out: 'logout',
+               registration: 'signup'
+             },
+
+             controllers: {
+               sessions: 'admins/sessions',
+               registrations: 'admins/registrations'
+             }
 
   mount GraphiQL::Rails::Engine, at: '/graphiql', graphql_path: '/graphql' if Rails.env.development?
   post '/graphql', to: 'graphql#execute'
