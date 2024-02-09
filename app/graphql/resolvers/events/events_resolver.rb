40817ccf22f30f20a6da8771a7b29c468b98f6cd
Prajwal Gautam <prajwalgautam08@gmail.com>
2024-02-09 17:29:02 +0545

    Fix: graphql resolvers for events and places fixed

--- app/graphql/resolvers/events/events_resolver.rb
+++ app/graphql/resolvers/events/events_resolver.rb
@@ -4,8 +4,8 @@ module Resolvers
       argument :skip, Int, required: false
       argument :take, Int, required: false
       type [Types::EventType], null: false
-      def resolve(skip, take)
-        if skip && take
+      def resolve(skip: 0, take: 0)
+        if skip && take != 0
           Event.order(:id).offset(skip).limit(take)
         else
           Event.order(:id).all
