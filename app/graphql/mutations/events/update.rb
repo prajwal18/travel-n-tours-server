8dab91225425ff366a1a1eafbb13405f358be949
Prajwal Gautam <prajwalgautam08@gmail.com>
2024-03-03 19:02:06 +0545

    Feat: add bannar and gallery image during editing event and places done

--- app/graphql/mutations/events/update.rb
+++ app/graphql/mutations/events/update.rb
@@ -5,16 +5,37 @@ module Mutations
       argument :name, String, required: false
       argument :description, String, required: false
       argument :place_id, ID, required: false
+      argument :banner, ApolloUploadServer::Upload, required: false
+      argument :pictures, [ApolloUploadServer::Upload], required: false
 
       type Types::EventType
 
-      def resolve(id:, description: nil, place_id: nil, name: nil)
+      def resolve(id:, description: nil, place_id: nil, name: nil, banner: nil, pictures: nil)
         event = Event.find(id)
 
         event.name = name if name
         event.description = description if description
-
         event.place_id = place_id if place_id
+
+        if banner 
+          event.banner.purge
+          banner_blob = ActiveStorage::Blob.create_and_upload!(
+            io: banner,
+            filename: banner.original_filename,
+            content_type: banner.content_type
+          )
+          event.banner = banner_blob
+        end
+
+        if pictures && !pictures.empty?
+          pictures.each do |picture|
+            event.pictures.attach(
+              io: picture,
+              filename: picture.original_filename,
+              content_type: picture.content_type
+            )
+          end
+        end
 
         event.save
         event
--- app/graphql/mutations/places/update.rb
+++ app/graphql/mutations/places/update.rb
@@ -4,14 +4,36 @@ module Mutations
       argument :id, ID, required: true
       argument :name, String, required: false
       argument :description, String, required: false
+      argument :banner, ApolloUploadServer::Upload, required: false
+      argument :pictures, [ApolloUploadServer::Upload], required: false
 
       type Types::PlaceType
 
-      def resolve(id:, name: nil, description: nil)
+      def resolve(id:, name: nil, description: nil, banner: nil, pictures: nil)
         place = Place.find(id)
 
         place.name = name if name
         place.description = description if description
+
+        if banner 
+          place.banner.purge
+          banner_blob = ActiveStorage::Blob.create_and_upload!(
+            io: banner,
+            filename: banner.original_filename,
+            content_type: banner.content_type
+          )
+          place.banner = banner_blob
+        end
+
+        if pictures && !pictures.empty?
+          pictures.each do |picture|
+            place.pictures.attach(
+              io: picture,
+              filename: picture.original_filename,
+              content_type: picture.content_type
+            )
+          end
+        end
 
         place.save
         place
