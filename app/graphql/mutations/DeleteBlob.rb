c526a2f9996058354b6b85cd056cba7c41dbe27f
Prajwal Gautam <prajwalgautam08@gmail.com>
2024-03-04 11:23:34 +0545

    Feat: Gallery images can now be deleted while editing events and places.

--- app/graphql/mutations/DeleteBlob.rb
+++ app/graphql/mutations/DeleteBlob.rb
@@ -1,15 +1,29 @@
 module Mutations
-    class DeleteBlob < GraphQL::Schema::Mutation
-      argument :url, String, required: true
+  class DeleteBlobPayload < Types::BaseObject
+    field :event, Types::EventType, null: true
+    field :place, Types::PlaceType, null: true
+    field :error, [String], null: true
+  end
 
-      type String
-      def resolve(url:)
-        blob_id = url.match(/\/([a-zA-Z0-9_-]+)\?/)[1]
-        blob = ActiveStorage::Blob.find_signed(blob_id)
-        blob.purge
-        "Image delete successfully"
-      rescue StandardError => e
-        "Failed to delete image. Sorry! try again"
-      end
+  class DeleteBlob < GraphQL::Schema::Mutation
+    argument :url, String, required: true
+
+    type DeleteBlobPayload
+    def resolve(url:)
+      regex = %r{active_storage/blobs/redirect/(.*)/}
+      signed_blob_id = url.match(regex)[1]
+      blob = ActiveStorage::Blob.find_signed(signed_blob_id)
+      # blob.purge is not working. That is why the below code is written
+      blob_id = blob.id
+      attachment = blob.attachments[0]
+      model = attachment.record_type
+      model_id = attachment.record_id
+      record = model.constantize.find(model_id)
+      record.pictures.find_by(blob_id:).purge
+      record.save
+      model == 'Place' ? { place: record } : { event: record }
+    rescue StandardError => _e
+      { error: 'Failed to delete the image' }
     end
+  end
 end
